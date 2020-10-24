80cf54ae3d5abcc8a685376ee490085e97b3b391
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-24 14:39:54 +0000

    Solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
+from item import Item
 def main():
     #write your code below this line
+    itemList = []
+    while(True):
+      string = input("Name: ")
+      if not string:
+        break
+      itemList.append(Item(string))
+    
+    for item in itemList:
+      print(item)
 
 if __name__ == '__main__':
     main()
